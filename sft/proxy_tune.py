33ecbf79f5fb40cbbcaf91741d023aba7dac4a7e
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-04-24 21:27:50 +0900

    baseline_impl

--- sft/proxy_tune.py
+++ sft/proxy_tune.py
@@ -2,7 +2,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 base_model_name = "llm-jp/llm-jp-3-980m"
-ft_model_name = "llm-jp/llm-jp-3-980m-instruct3"
+sft_model_name = "llm-jp/llm-jp-3-3.7B-instruct3"
 untune_model_name = "llm-jp/llm-jp-3-980m"
 # device_map をサンプルの時点から実装してるのはとても偉いと思う（当たり前か）
 
@@ -14,15 +14,15 @@ def load_model(model_name):
     return model
 
 base_model = load_model(base_model_name)
-ft_model = load_model(ft_model_name)
+sft_model = load_model(sft_model_name)
 untune_model = load_model(untune_model_name)
 
+# base と sft_tokenizer で ChatTemplate の有無があるので分離
 base_tokenizer = AutoTokenizer.from_pretrained(base_model_name)
-ft_tokenizer = AutoTokenizer.from_pretrained(ft_model_name)
+sft_tokenizer = AutoTokenizer.from_pretrained(sft_model_name)
 
-# 元々は英語だが，何言ってるかわからんので日本語に変更した
 prompt = "次の文に続く文を生成してください．\n吾輩は猫である．"
-jp_messages = [
+sft_messages = [
     {
         "role": "system",
         "content": "以下は、タスクを説明する指示です。要求を適切に満たす応答を書きなさい。",
@@ -30,44 +30,67 @@ jp_messages = [
     {"role": "user", "content": prompt},
 ]
 
-jp_text = ft_tokenizer.apply_chat_template(
-    jp_messages, tokenize=False, add_generation_prompt=True
+sft_text = sft_tokenizer.apply_chat_template(
+    sft_messages, tokenize=False, add_generation_prompt=True
 )
 
-max_length = 50
+max_length = 20
 
-jp_model_inputs = ft_tokenizer([jp_text], return_tensors="pt").to(base_model.device)
-jp_input_ids = jp_model_inputs["input_ids"]
-jp_eos_token_id = ft_tokenizer.eos_token_id
+sft_model_inputs = sft_tokenizer([sft_text], return_tensors="pt").to(base_model.device)
+sft_input_ids = sft_model_inputs["input_ids"]
+sft_eos_token_id = sft_tokenizer.eos_token_id
 
 
 def inf_tokens_logits(tokenizer, model, input_ids, max_length):
     tokens = []
-    prob_list = []
-    jp_generating = True
+    candidate_tokens_list = []
+    candidate_logits_list = []
+    logits_list = []
+    sft_generating = True
 
     for i in range(max_length):
-        if jp_generating:
+        if sft_generating:
             output = base_model(input_ids)
+
             next_token_logits = output.logits[0, -1, :]
             sorted_ids = torch.argsort(next_token_logits, descending=True)
+
             next_token_id = sorted_ids[0]
-            prob_list.append(next_token_logits[next_token_id].item())
+            candidate_token_ids = sorted_ids[:3]
+
+            candidate_logits = [next_token_logits[id].item() for id in candidate_token_ids]
+            candidate_logits_list.append(candidate_logits)
+            candidate_tokens = [tokenizer.decode(id) for id in candidate_token_ids]
+            candidate_tokens_list.append(candidate_tokens)
+
+            logits_list.append(next_token_logits[next_token_id].item())
             next_token = tokenizer.decode(next_token_id)
             tokens.append(next_token)
-            if next_token_id.item() == jp_eos_token_id:
-                jp_generating = False
+
+            # EOS ならば，生成終了
+            if next_token_id.item() == sft_eos_token_id:
+                sft_generating = False
+
+            # これまでの入力に次のトークンを繋げてループ先頭へ
             input_ids = torch.cat(
                 [input_ids, next_token_id.unsqueeze(0).unsqueeze(0)], dim=-1
             )
 
-    return tokens, prob_list
+    return tokens, logits_list, candidate_tokens_list, candidate_logits_list
 
-tokens, prob_list = inf_tokens_logits(ft_tokenizer, ft_model, jp_input_ids, max_length)
+tokens, logits_list, candidtate_tokens_list, candidate_logits_list = \
+    inf_tokens_logits(sft_tokenizer, sft_model, sft_input_ids, max_length)
+
 print(tokens)
-print(prob_list)
+print(logits_list)
 
-for t,p in zip(tokens, prob_list):
+for t,p in zip(tokens, logits_list):
     st = str(t)
     sp = str(p)
     print(st+'\t'+sp)
+
+
+for ct,cp in zip(candidtate_tokens_list, candidate_logits_list):
+    sct = ',\t'.join([str(token) for token in ct])
+    scp = ',\t'.join([str(prob) for prob in cp])
+    print(sct+'\t'+scp)
