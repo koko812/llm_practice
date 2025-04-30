176de25d4903e2a37d39184f7321cf983a87a358
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-05-01 00:37:23 +0900

    sft_starting

--- sft/sft.py
+++ sft/sft.py
@@ -1,2 +1,23 @@
-import transformers 
-print('9')
+from datasets import load_dataset
+from trl import SFTConfig, SFTTrainer
+import os
+
+os.environ["WANDB_PROJECT"] = "<my-amazing-project>"  # W&Bプロジェクトの名前を指定
+os.environ["WANDB_LOG_MODEL"] = "checkpoint"  # すべてのモデルチェックポイントをログ
+
+dataset = load_dataset("stanfordnlp/imdb", split="train")
+
+training_args = SFTConfig(
+    max_length=512,
+    output_dir="tmp",
+    report_to="wandb",
+    save_total_limit=3
+)
+
+# adnis だと，モデル一つに乗り切らなかった，残念
+trainer = SFTTrainer(
+    "facebook/opt-350m",
+    train_dataset=dataset,
+    args=training_args,
+)
+trainer.train()
